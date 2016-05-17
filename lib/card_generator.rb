94e232bac77c568068e10d987b2fb25f8c38e64e
Jason Hanna <jason.hanna01@gmail.com>
2016-05-17 14:47:10 -0600

    able to split array properly

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,15 +1,21 @@
 require_relative 'card'
 
 class CardGenerator
-  attr_reader :cards, :new_card
+  attr_reader :cards
+  attr_accessor :new_card
 
   def initialize
     @cards = File.read(ARGV[0])
+    @new_card = []
   end
 
   def to_object
-    card = @cards.split("\n")
-    @new_card = card.each_slice(1).to_a
+    #lines = @cards.split("\n")
+    @cards.each_line do |line|
+      @new_card << line.chomp.split(",")
+
+    end
+    @new_card
   end
 end
 
