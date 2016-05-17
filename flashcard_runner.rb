ef4b77c2243ec58f1c825df20b95fd8dac26721f
Jason Hanna <jason.hanna01@gmail.com>
2016-05-17 11:31:55 -0600

    completed runner

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,11 +3,11 @@ require_relative './lib/card'
 require_relative './lib/guess'
 require_relative './lib/round'
 
-card_1 = Card.new("What is the capital of Alaska?", "Juneau")
-card_2 = Card.new("Approximately how many miles are in one astronomical unit?", "93,000,000")
+card_1 = Card.new("What is the best bar on Blake St?", "The Vault")
+card_2 = Card.new("Who won the 2016 NCAA National Championship?", "Villanova")
 card_3 = Card.new("Who is the best player in the NBA?", "Kristaps Porzingis")
-card_4 = Card.new("Who is the MVP of the NBA?", "Steph Curry")
-card_5 = Card.new("Who is the President of the Knicks?", "Phil Jackson")
+card_4 = Card.new("Who is the President of the Knicks?", "Phil Jackson")
+card_5 = Card.new("Clear eyes, full hearts?", "Can't Lose")
 deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
 round = Round.new(deck)
 
--- lib/round.rb
+++ lib/round.rb
@@ -19,6 +19,7 @@ class Round
   def record_guess(response)
     @number_correct += 1 if response == current_card.answer
     @guesses << Guess.new(response, current_card)
+    p guesses.last.feedback
   end
 
   def percent_correct
@@ -28,7 +29,22 @@ class Round
 
   def start
     p "Welcome! You are playing with #{deck.count} cards"
+    p "----------------------------------------------------"
+    while current_card != nil
+      card_update = "This is card #{guesses.count + 1} out of #{deck.count}"
+    p card_update
+    p current_card.question
+    repl_response
   end
+  p "****** Game over! ******"
+  p "You had #{number_correct} correct guesses out of #{guesses.count} for a score of #{(percent_correct * 100).to_i}%."
+  end
+
+  def repl_response
+    record_guess(gets.chomp)
+    @guesses.last.feedback
+  end
+
 
 
 
